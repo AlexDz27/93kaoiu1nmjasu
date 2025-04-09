f8ee90eea75ef6027229e633dadbde3fd4685e92
Alex Dzyuba <alexeydzyuba27@gmail.com>
2025-04-09 21:31:45 +0300

    fin slider q

--- js/main.js
+++ js/main.js
@@ -2,24 +2,29 @@
 const track = document.getElementById('track')
 const btnPrev = document.getElementById('btnPrev')
 const btnNext = document.getElementById('btnNext')
-const cardWidth = track.firstElementChild.clientWidth // TODO: del)
-// const cardWidth = 385.33
-const trackWidth = track.clientWidth
+const CARD_AND_GAP_WIDTH = 407.5 // .slider__track's children grid-auto-columns + column-gap
 let multiplier = 0
 btnPrev.addEventListener('click', () => {
-  console.log('prev')
+  multiplier--
+  btnNext.disabled = false
+  if (multiplier === 0) {
+    btnPrev.classList.add('btn-slider--inactive')
+    btnPrev.disabled = true
+  }
+
+  track.style.transform = `translate3d(-${CARD_AND_GAP_WIDTH * multiplier}px, 0, 0)`
+
+  btnNext.classList.remove('btn-slider--inactive')
 })
 btnNext.addEventListener('click', () => {
   multiplier++
+  btnPrev.disabled = false
+  if (multiplier === 2) {
+    btnNext.classList.add('btn-slider--inactive')
+    btnNext.disabled = true
+  }
 
-  // track.style.transform = `translate3d(-${385 * multiplier}px, 0, 0)`
-  // track.style.transform = `translate3d(calc(-(100% - 44px) / 3)px, 0, 0)`
-  // track.style.transform = `translate3d(calc(-((100% - 44px) / 3)), 0, 0)`
-
-  // working
-  // track.style.transform = `translate3d(calc((-${trackWidth}px - 44px) / 3), 0, 0)`
-  // working better
-  track.style.transform = `translate3d(calc(-${cardWidth * multiplier}px - 22px), 0, 0)`
+  track.style.transform = `translate3d(-${CARD_AND_GAP_WIDTH * multiplier}px, 0, 0)`
 
   btnPrev.classList.remove('btn-slider--inactive')
 })
