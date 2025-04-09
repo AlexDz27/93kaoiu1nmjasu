54b283bb7ae46ed0f2efb43586162857d8deb1cb
Alex Dzyuba <alexeydzyuba27@gmail.com>
2025-04-09 21:08:04 +0300

    slider start doing something

--- js/main.js
+++ js/main.js
@@ -2,6 +2,9 @@
 const track = document.getElementById('track')
 const btnPrev = document.getElementById('btnPrev')
 const btnNext = document.getElementById('btnNext')
+const cardWidth = track.firstElementChild.clientWidth // TODO: del)
+// const cardWidth = 385.33
+const trackWidth = track.clientWidth
 let multiplier = 0
 btnPrev.addEventListener('click', () => {
   console.log('prev')
@@ -11,7 +14,12 @@ btnNext.addEventListener('click', () => {
 
   // track.style.transform = `translate3d(-${385 * multiplier}px, 0, 0)`
   // track.style.transform = `translate3d(calc(-(100% - 44px) / 3)px, 0, 0)`
-  track.style.transform = `translate3d(calc(-((100% - 44px) / 3)), 0, 0)`
+  // track.style.transform = `translate3d(calc(-((100% - 44px) / 3)), 0, 0)`
+
+  // working
+  // track.style.transform = `translate3d(calc((-${trackWidth}px - 44px) / 3), 0, 0)`
+  // working better
+  track.style.transform = `translate3d(calc(-${cardWidth * multiplier}px - 22px), 0, 0)`
 
   btnPrev.classList.remove('btn-slider--inactive')
 })
