1ab8fed0a9917c80078582940aefbde96c7f12bb
Alex Dzyuba <alexeydzyuba27@gmail.com>
2025-05-06 18:37:22 +0300

    w; seems like got state before cycling

--- js/slider.js
+++ js/slider.js
@@ -3,34 +3,59 @@ class Slider {
     this.track = document.getElementById(trackId)
     this.btnPrev = document.getElementById(btnPrevId)
     this.btnNext = document.getElementById(btnNextId)
-    this.windowPos = [[1, 2, 3], 4, 5]
-    // this.windowPos2 = [1, [2, 3, 4], 5] // TODO: del
 
+    this._windowPos = 0 // Notion of 'window' represents viewing window for the cards, e.g. [[1, 2, 3], 4, 5,], and it could be [[1, 2], 3, 4, 5] for mobile
+
+    this.WINDOW_POS_MAX = 2
     this.CHANGE_SLIDE_DISTANCE = 407.5 // .slider__track's children grid-auto-columns + column-gap
+
+    this.init()
   }
 
-  slide(newWindowPos) {
-    // track.style.transform = `translate3d(-${CARD_AND_GAP_WIDTH * slideWindowCount}px, 0, 0)`
+  init() {
+    this.btnPrev.onclick = () => {
+      this.slidePrev()
+      console.log(this.windowPos)
+    }
+    this.btnNext.onclick = () => {
+      this.slideNext()
+      console.log(this.windowPos)
+    }
+  }
+
+  slidePrev() {
+    this.windowPos--
+    this._slide(this.windowPos)
+  }
+  slideNext() {
+    this.windowPos++
+    this._slide(this.windowPos)
+  }
+
+  _slide(windowPos) {
+    this.track.style.transform = `translate3d(-${this.CHANGE_SLIDE_DISTANCE * windowPos}px, 0, 0)`
+  }
+
+  get windowPos() {
+    return this._windowPos
+  }
+  set windowPos(val) {
+    this._windowPos = clamp(val, 0, this.WINDOW_POS_MAX)
+
+    if (val > 0) {
+      this.btnPrev.disabled = false
+      this.btnPrev.classList.remove('btn-slider--inactive')
+    } else if (val === 0) {
+      this.btnPrev.disabled = true
+      this.btnPrev.classList.add('btn-slider--inactive')
+    }
   }
 }
 
-// const slider = new Slider()
-// slider.echo()
+const slider = new Slider('track', 'btnPrev', 'btnNext')
+console.log(slider)
 
-// TESTING
-const ar1 = [[1, 2, 3], 4, 5]
-const ar2 = [1, [2, 3, 4], 5]
-const ar3 = [1, 2, [3, 4, 5]]
-const ar4 = [1, 2, [3, 4, 5], 6, 7, 8, 9, 10]
-const ar5 = [1, 2, 3, [4, 5, 6], 7, 8, 9, 10]
-const ar6 = [1, 2, 3, 4, [5, 6, 7], 8, 9, 10]
-const ar61 = [1, 2, 3, 4, 5, [6, 7, 8], 9, 10]
-const ar7 = [1, 2, 3, 4, 5, 6, 7, [8, 9, 10]]
-console.log(ar1.findIndex(i => Array.isArray(i))) // 0
-console.log(ar2.findIndex(i => Array.isArray(i))) // 1
-console.log(ar3.findIndex(i => Array.isArray(i))) // 2
-console.log(ar4.findIndex(i => Array.isArray(i))) // 2
-console.log(ar5.findIndex(i => Array.isArray(i))) // 3
-console.log(ar6.findIndex(i => Array.isArray(i))) // 4
-console.log(ar61.findIndex(i => Array.isArray(i))) // 5
-console.log(ar7.findIndex(i => Array.isArray(i))) // 7
+/* Utilities for Slider */
+function clamp(val, min, max) {
+  return Math.min(Math.max(val, min), max)
+}
