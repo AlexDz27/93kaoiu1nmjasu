42ed07a63d8c3ae8bf8756dc15c4120089ae6e75
Alex Dzyuba <alexeydzyuba27@gmail.com>
2025-05-06 16:50:00 +0300

    kinda poc

--- js/main.js
+++ js/main.js
@@ -69,56 +69,56 @@ if (track) {
   const btnPrev = document.getElementById('btnPrev')
   const btnNext = document.getElementById('btnNext')
   const CARD_AND_GAP_WIDTH = 407.5 // .slider__track's children grid-auto-columns + column-gap
-  // const CLONE_PROGRESSION_SET = new Set([2, 7, 12, 17, 22, 27, 32, 37, 42, 47, 52, 57, 62, 67, 72, 77, 82, 87, 92, 97])
-  const CLONE_PROGRESSION_SET = new Set([2, 7, 12]) // TODO: add 17 and check with last plus 5
-  const CLONE_PROGRESSION_SET_LAST = 12
-  const DELETE_PROGRESSION_SET = new Set([8, 18, 28, 38])
-  let multiplier = 0
+  let slideWindowCount = 0
   btnPrev.addEventListener('click', () => {
-    multiplier--
+    slideWindowCount--
     btnNext.disabled = false
-    if (multiplier === 0) {
+    if (slideWindowCount === 0) {
       btnPrev.classList.add('btn-slider--inactive')
       btnPrev.disabled = true
     }
 
-    track.style.transform = `translate3d(-${CARD_AND_GAP_WIDTH * multiplier}px, 0, 0)`
+    track.style.transform = `translate3d(-${CARD_AND_GAP_WIDTH * slideWindowCount}px, 0, 0)`
 
     btnNext.classList.remove('btn-slider--inactive')
   })
   btnNext.addEventListener('click', () => {
-    multiplier++
+    slideWindowCount++
     btnPrev.disabled = false
-    console.log(multiplier)
-    if (multiplier === (CLONE_PROGRESSION_SET_LAST + 5)) {
-      btnNext.classList.add('btn-slider--inactive')
-      btnNext.disabled = true
-    }
-
-    if (CLONE_PROGRESSION_SET.has(multiplier)) {
+    console.log(slideWindowCount)
+    if (slideWindowCount === 2) {
       const fragment = document.createDocumentFragment()
-      for (const card of cards) fragment.appendChild(card.cloneNode(true))
+      let lastCardId = 5
+      for (const card of cards) {
+        const clone = card.cloneNode(true)
+        clone.dataset.cardId = ++lastCardId
+        fragment.appendChild(clone)
+      }
       track.appendChild(fragment)
-      console.log('done')
+      console.log('done cloning')
     }
-    // if (DELETE_PROGRESSION_SET.has(multiplier)) {
-    //   for (let i = 0; i < 5; i++) {
-    //     // console.log(track.children[i])
-    //     track.children[i].remove()
-    //   }
-    // }
+    if (slideWindowCount === 5) {
+      track.style.transform = `translate3d(-${CARD_AND_GAP_WIDTH * slideWindowCount}px, 0, 0)`
+      setTimeout(() => {
+        for (i = 0; i < 5; i++) {
+          track.firstElementChild.remove()
+        }
+        track.style.transition = 'none'
+        track.style.transform = `translate3d(0px, 0, 0)`
+        slideWindowCount = 0
+      }, 800)
+      setTimeout(() => {
+        track.style.transition = 'transform 400ms cubic-bezier(0.165, 0.84, 0.44, 1)'
+      }, 900)
 
-    track.style.transform = `translate3d(-${CARD_AND_GAP_WIDTH * multiplier}px, 0, 0)`
+      // slideWindowCount = 0
+      // btnPrev.classList.add('btn-slider--inactive')
+      // btnPrev.disabled = true
+      return
+    }
+
+    track.style.transform = `translate3d(-${CARD_AND_GAP_WIDTH * slideWindowCount}px, 0, 0)`
 
     btnPrev.classList.remove('btn-slider--inactive')
   })
-  function isMemberOfCloneProgression(num) {
-    if (num === 2) return true
-    return (num + 5) === arProg()
-  }
-  function arProg(num) {
-    if (num === 1) return false
-    if (num === 2) return true
-    return 5 * num - 3
-  }
 }
