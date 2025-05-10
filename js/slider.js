ae3e4f8bfdb4c60a8af4aba9c21b7a8e4bd1ff20
Alex Dzyuba <alexeydzyuba27@gmail.com>
2025-05-10 12:29:21 +0300

    slider kinda work as needed; todo: improve and ref

--- js/slider.js
+++ js/slider.js
@@ -4,9 +4,12 @@ class Slider {
     this.btnPrev = document.getElementById(btnPrevId)
     this.btnNext = document.getElementById(btnNextId)
 
-    this._windowPos = 0 // Notion of 'window' represents viewing window for the cards, e.g. [[1, 2, 3], 4, 5,], and it could be [[1, 2], 3, 4, 5] for mobile
+    this.cards = this.track.children
+    this.initCardsCount = this.cards.length
 
+    this._windowPos = 0 // Notion of 'window' represents viewing window for the cards, e.g. [[1, 2, 3], 4, 5,], and it could be [[1, 2], 3, 4, 5] for mobile
     this.WINDOW_POS_MAX = 2
+
     this.CHANGE_SLIDE_DISTANCE = 407.5 // .slider__track's children grid-auto-columns + column-gap
 
     this.init()
@@ -30,6 +33,36 @@ class Slider {
   slideNext() {
     this.windowPos++
     this._slide(this.windowPos)
+
+    if (this.windowPos === this.WINDOW_POS_MAX) {
+      const fragment = document.createDocumentFragment()
+      let lastCardId = 5 // TODO: del
+      for (const card of this.cards) {
+        const clone = card.cloneNode(true)
+        clone.dataset.cardId = ++lastCardId // TODO: del
+        fragment.appendChild(clone)
+      }
+      this.track.appendChild(fragment)
+      console.log('done cloning')
+    }
+
+    
+    if (this.windowPos === this.initCardsCount) {
+      const transHandler = () => {
+        for (let i = 0; i < 5; i++) {
+          this.track.firstElementChild.remove()
+        }
+        this.track.style.transition = 'none'
+        this.track.style.transform = `translate3d(0px, 0, 0)`
+        this.windowPos = 0
+      }
+      this.track.addEventListener('transitionend', transHandler)
+
+      setTimeout(() => {
+        this.track.style.transition = 'transform 400ms cubic-bezier(0.165, 0.84, 0.44, 1)'
+        this.track.removeEventListener('transitionend', transHandler)
+      }, 450)
+    }
   }
 
   _slide(windowPos) {
@@ -40,12 +73,12 @@ class Slider {
     return this._windowPos
   }
   set windowPos(val) {
-    this._windowPos = clamp(val, 0, this.WINDOW_POS_MAX)
+    this._windowPos = getNonNegative(val)
 
-    if (val > 0) {
+    if (this._windowPos > 0) {
       this.btnPrev.disabled = false
       this.btnPrev.classList.remove('btn-slider--inactive')
-    } else if (val === 0) {
+    } else if (this._windowPos === 0) {
       this.btnPrev.disabled = true
       this.btnPrev.classList.add('btn-slider--inactive')
     }
@@ -56,6 +89,6 @@ const slider = new Slider('track', 'btnPrev', 'btnNext')
 console.log(slider)
 
 /* Utilities for Slider */
-function clamp(val, min, max) {
-  return Math.min(Math.max(val, min), max)
+function getNonNegative(num) {
+  return Math.max(0, num);
 }
