51c3ccf6a1faf3fe8bc93e2efbbe9b7d94593744
Alex Dzyuba <alexeydzyuba27@gmail.com>
2025-05-10 16:32:27 +0300

    kinda finish adap

--- js/slider.js
+++ js/slider.js
@@ -1,8 +1,10 @@
 class Slider {
-  constructor(trackId, btnPrevId, btnNextId) {
+  constructor(trackId, btnPrevId, btnNextId, btnPrevMob, btnNextMob) {
     this.track = document.getElementById(trackId)
     this.btnPrev = document.getElementById(btnPrevId)
+    this.btnPrevMob = document.getElementById(btnPrevMob)
     this.btnNext = document.getElementById(btnNextId)
+    this.btnNextMob = document.getElementById(btnNextMob)
 
     this.cards = this.track.children
     this.initCardsCount = this.cards.length
@@ -10,7 +12,47 @@ class Slider {
     this._windowPos = 0 // Notion of 'window' represents viewing window for the cards, e.g. [[1, 2, 3], 4, 5,], and it could be [[1, 2], 3, 4, 5] for mobile
     this.WINDOW_POS_MAX = 2
 
-    this.CHANGE_SLIDE_DISTANCE = 407.5 // .slider__track's children grid-auto-columns + column-gap
+    // FIXME: mb i can do the same as _windowPos kinda..
+    this.changeSlideDistance = 407.5 // .slider__track's children grid-auto-columns + column-gap
+    if (window.innerWidth <= 1365) {
+      this.changeSlideDistance = 340.5
+    }
+    if (window.innerWidth <= 1168) {
+      this.changeSlideDistance = 288.5
+    }
+    if (window.innerWidth <= 812) {
+      this.changeSlideDistance = 229
+    }
+    if (window.innerWidth <= 670) {
+      this.changeSlideDistance = 243
+    }
+    if (window.innerWidth <= 577) {
+      this.changeSlideDistance = 235
+    }
+    if (window.innerWidth <= 540) {
+      this.changeSlideDistance = 210
+    }
+    window.addEventListener('resize', () => {
+      this.changeSlideDistance = 407.5
+      if (window.innerWidth <= 1365) {
+        this.changeSlideDistance = 340.5
+      }
+      if (window.innerWidth <= 1168) {
+        this.changeSlideDistance = 288.5
+      }
+      if (window.innerWidth <= 812) {
+        this.changeSlideDistance = 229
+      }
+      if (window.innerWidth <= 670) {
+        this.changeSlideDistance = 243
+      }
+      if (window.innerWidth <= 577) {
+        this.changeSlideDistance = 235
+      }
+      if (window.innerWidth <= 540) {
+        this.changeSlideDistance = 210
+      }
+    })
 
     this.init()
   }
@@ -18,12 +60,36 @@ class Slider {
   init() {
     this.btnPrev.onclick = () => {
       this.slidePrev()
-      console.log(this.windowPos)
     }
+    this.btnPrevMob.onclick = () => {
+      this.slidePrev()
+    }
+
     this.btnNext.onclick = () => {
       this.slideNext()
-      console.log(this.windowPos)
+      this.btnNext.disabled = true // [1] hack to prevent going too much farher when clicking fast
     }
+    this.btnNextMob.onclick = () => {
+      this.slideNext()
+      this.btnNextMob.disabled = true
+    }
+
+    this.track.addEventListener('transitionend', () => {
+      this.btnNext.disabled = false // [2] hack to prevent going too much farher when clicking fast
+      this.btnNextMob.disabled = false
+
+      if (this.windowPos === this.initCardsCount) {
+        for (let i = 0; i < 5; i++) {
+          this.track.firstElementChild.remove()
+        }
+        this.track.style.transition = 'none'
+        this.track.style.transform = `translate3d(0px, 0, 0)`
+        this.windowPos = 0
+
+        void this.track.offsetWidth // force reflow. Without this, line below won't work
+        this.track.style.transition = 'transform 400ms cubic-bezier(0.165, 0.84, 0.44, 1)'
+      }
+    })
   }
 
   slidePrev() {
@@ -36,37 +102,16 @@ class Slider {
 
     if (this.windowPos === this.WINDOW_POS_MAX) {
       const fragment = document.createDocumentFragment()
-      let lastCardId = 5 // TODO: del
       for (const card of this.cards) {
         const clone = card.cloneNode(true)
-        clone.dataset.cardId = ++lastCardId // TODO: del
         fragment.appendChild(clone)
       }
       this.track.appendChild(fragment)
-      console.log('done cloning')
-    }
-
-    
-    if (this.windowPos === this.initCardsCount) {
-      const transHandler = () => {
-        for (let i = 0; i < 5; i++) {
-          this.track.firstElementChild.remove()
-        }
-        this.track.style.transition = 'none'
-        this.track.style.transform = `translate3d(0px, 0, 0)`
-        this.windowPos = 0
-      }
-      this.track.addEventListener('transitionend', transHandler)
-
-      setTimeout(() => {
-        this.track.style.transition = 'transform 400ms cubic-bezier(0.165, 0.84, 0.44, 1)'
-        this.track.removeEventListener('transitionend', transHandler)
-      }, 450)
     }
   }
 
   _slide(windowPos) {
-    this.track.style.transform = `translate3d(-${this.CHANGE_SLIDE_DISTANCE * windowPos}px, 0, 0)`
+    this.track.style.transform = `translate3d(-${this.changeSlideDistance * windowPos}px, 0, 0)`
   }
 
   get windowPos() {
@@ -77,16 +122,19 @@ class Slider {
 
     if (this._windowPos > 0) {
       this.btnPrev.disabled = false
+      this.btnPrevMob.disabled = false
       this.btnPrev.classList.remove('btn-slider--inactive')
+      this.btnPrevMob.classList.remove('btn-slider--inactive')
     } else if (this._windowPos === 0) {
       this.btnPrev.disabled = true
+      this.btnPrevMob.disabled = true
       this.btnPrev.classList.add('btn-slider--inactive')
+      this.btnPrevMob.classList.add('btn-slider--inactive')
     }
   }
 }
 
-const slider = new Slider('track', 'btnPrev', 'btnNext')
-console.log(slider)
+const slider = new Slider('track', 'btnPrev', 'btnNext', 'btnPrevMob', 'btnNextMob')
 
 /* Utilities for Slider */
 function getNonNegative(num) {
