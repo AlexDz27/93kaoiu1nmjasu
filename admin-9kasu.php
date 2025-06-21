7b69a6c9bff5d966427b2d71798b02aff3935d78
Alex Dzyuba <alexeydzyuba27@gmail.com>
2025-06-21 11:17:59 +0300

    w

--- admin-9kasu.php
+++ admin-9kasu.php
@@ -1,3 +1,9 @@
+<?php
+
+$db = require 'db.php';
+$products = $db['products']['sprava'];
+
+?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -11,14 +17,30 @@
 
 <main class="main cont">
   <h1 style="margin-bottom: 60px;">Панель администратора</h1>
-  <h2>Обновить прайс-лист:</h2>
-  <form id="loadFileForm" method="POST" enctype="multipart/form-data">
-    <p>
-      <label for="file">Выберите файл прайс-листа: &nbsp;</label>
-      <input name="file" type="file" id="file">
-    </p>
-    <button id="loadBtn" class="btn-load" type="submit">Загрузить файл прайс-листа</button>
-  </form>
+  <section class="section">
+    <h2>Обновить прайс-лист:</h2>
+    <form id="loadFileForm" method="POST" enctype="multipart/form-data">
+      <p>
+        <label for="file">Выберите файл прайс-листа: &nbsp;</label>
+        <input name="file" type="file" id="file">
+      </p>
+      <button id="loadBtn" class="btn-load" type="submit">Загрузить файл прайс-листа</button>
+    </form>
+  </section>
+
+  <section class="section">
+    <h2>Редактировать товары:</h2>
+    <div class="products-grid">
+      <?php foreach ($products as $product): ?>
+        <a href="javascript:void(0)" class="products-grid__product">
+          <img class="products-grid__product__img" src="<?= $product['img'] ?>" alt="">
+          <b><?= $product['model'] . ', ' . $product['variant'] ?></b>
+          <span class="products-grid__product__art">(<?= $product['art'] ?>)</span>
+          <button class="btn-edit products-grid__product__btn-edit">Редактировать</button>
+        </a>
+      <?php endforeach ?>
+    </div>
+  </section>
 </main>
   
 </body>
