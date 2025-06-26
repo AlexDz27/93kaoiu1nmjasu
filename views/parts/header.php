f21f3f98f435bab4c488dc9c87428111ab561c46
Alex Dzyuba <alexeydzyuba27@gmail.com>
2025-06-26 12:22:17 +0300

    w

--- views/parts/header.php
+++ views/parts/header.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="icon" type="image/jpg" href="favicon.ico">
+  <link rel="icon" type="image/jpg" href="/favicon.ico">
   <!-- TODO: r this -->
   <meta name="robots" content="noindex,nofollow,noarchive">
   <link rel="stylesheet" href="/css/style-v10.css">
