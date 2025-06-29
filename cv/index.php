080862dfc14c9246bd8568d13bbd3992c68b5b2e
Alex Dzyuba <alexeydzyuba27@gmail.com>
2025-06-29 19:13:15 +0300

    cv start

---
+++ cv/index.php
@@ -0,0 +1,21 @@
+<?php
+
+$title = 'PHP Developer';
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="css/main.css">
+  <title>Алексей Дзюба – <?= $title ?></title>
+</head>
+<body>
+
+<main class="main cont">
+  <p>Hello there</p>
+</main>
+  
+</body>
+</html>
